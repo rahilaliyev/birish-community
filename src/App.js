08906a7dcc3be1696a4888ba1a0a3bd2cd7c96f4
Rahil <rahil.eliyev@gmail.com>
2022-01-09 23:25:10 +0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/styles/fonts.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
